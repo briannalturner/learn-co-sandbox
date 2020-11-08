666b80893065612600094855e5a3614f1d26c504
Bri Turner <blturner3527@gmail.com>
2020-11-08 18:08:10 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,52 @@
+def join_ingredients(src)
+new_arr = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    new_arr << "I love #{src[row_index][element_index]} and #{src[row_index][element_index + 1]} on my pizza"
+    row_index +=1
+  end
+  new_arr
+end
+
+arr = [[2,6],[4,5],[9,1],[6,8]]
+
+
+def find_greater_pair(src)
+  outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+    while element_index < src[row_index].count do
+      inner_results << src[row_index].max
+    end
+    outer_results << inner_results
+    outer_results
+  end
+end
+
+def total_even_pairs(src)
+  # if both numbers in the pair are even, then add both those numbers to the total
+  
+  total = 0
+  row_index = 0
+  
+  while row_index < src.length do
+    element_index = 0
+    
+    while element_index < src[row_index].count do
+      if (src[row_index][element_index]%2 == 0 && src[row_index][(element_index + 1)]%2 == 0)
+        total = total + src[row_index][element_index] + src[row_index][(element_index + 1)]
+      end
+    end
+  end
+  total
+end
+
+puts total_even_pairs(arr)
+
+
+
+
+
